5260d4087e8ac9e03ab85958362367d625d9bc55
Michal Vala <mvala@redhat.com>
2022-03-03 15:06:14 +0100

    some more greatness

--- env.sh
+++ env.sh
@@ -2,15 +2,23 @@
 
 set +x
 
+SCRIPT_DIR=$( dirname "${0}" )
+
 DEVDIR=${DEVDIR:-${HOME}/dev}
 PRIVATE_DOCKERREGISTRY=${PRIVATE_DOCKERREGISTRY:-quay.io/mvala}
 
 SPIOPERATORDIR=${DEVDIR}/spi-operator
 SPIOPERATORIMAGE=${PRIVATE_DOCKERREGISTRY}/spi-operator
 
+SPIOAUTHDIR=${DEVDIR}/spi-oauth
+SPIOAUTHIMAGE=${PRIVATE_DOCKERREGISTRY}/spi-oauth
 
 export SHARED_SECRET=blabol
 
 getBranch() {
-  git br --show-current
+  BRANCH=$( git branch --show-current )
+  if [ -z ${BRANCH} ]; then
+    BRANCH=$( cat .git/HEAD )
+  fi
+  echo ${BRANCH}
 }
